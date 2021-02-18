d04613110b11b4905b99cd7d5e01829a491594d6
n8 <bamboozal@insurance.kek>
2021-02-17 20:15:12 -0800

    updating local_pipeline.sh

--- alive/global/local_pipeline.sh
+++ alive/global/local_pipeline.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
 
-terraform init -reconfigure
+terraform init -reconfigure >/dev/null 2>&1
 
-if ! terraform fmt -check; then 
+if ! terraform fmt -check >/dev/null 2>&1; then 
     echo "tisk tisk"
-    terraform fmt
+    terraform fmt >/dev/null 2>&1
 fi
 
 terraform validate
-terraform plan -out=plan
+terraform plan
